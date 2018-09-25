4ba9a35a31bc29ed4e48837f291054ac2e780c5d
udugam <ryan.udugam@gmail.com>
2018-09-24 20:46:39 -0400

    refactored schema to a seperate file, added mutation Type

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 const {GraphQLServer} = require("graphql-yoga")
+
+let links = [{
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Full Stack Tutorial for GraphQL"
+}, {
+    id: "link-1",
+    url: "www.rhulabs.com",
+    description: "Full Stack Tutorial for GraphQL implementation"
+}]
+
+
+const resolvers = {
+    Query: {
+        info: () => `This is a test API`,
+        feed: () => links
+    },
+
+    Link: {
+        id: (root) => root.id,
+        description: (root) => root.description,
+        url: (root) => root.url
+    }
+
+}
+
+const server = new GraphQLServer({
+    typeDefs: './schema.graphql',
+    resolvers
+})
+
+server.start(() => console.log("Server is running on localhost port 4000")) 
